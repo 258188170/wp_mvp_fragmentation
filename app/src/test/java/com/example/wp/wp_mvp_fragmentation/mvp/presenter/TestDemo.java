cce7f75dd42efd7dd15d20013af5f77c92ea6539
wangpeng <wangpeng>
2018-06-07 09:30:44 +0800

    wp
    test

--- app/src/test/java/com/example/wp/wp_mvp_fragmentation/mvp/presenter/TestDemo.java
+++ app/src/test/java/com/example/wp/wp_mvp_fragmentation/mvp/presenter/TestDemo.java
@@ -7,15 +7,20 @@ import org.junit.Test;
  */
 public class TestDemo {
 
+    //翻转字符串
     @Test
     public void demo1() {
         String str = "wangpeng";
-        int len = str.length();
-        char[] array = str.toCharArray();
-        for (int i = 0; i < len / 2; i++) {
-            array[i] = str.charAt(len - 1 - i);
-            array[len - 1 - i] = str.charAt(i);
+//        int len = str.length();
+//        char[]array = str.toCharArray();
+//        for (int i = 0; i < len / 2; i++) {
+//            array[i] = str.charAt(len-1-i);
+//            array[len-1-i] = str.charAt(i);
+//        }
+        StringBuffer buffer = new StringBuffer();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            buffer.append(str.charAt(i));
         }
-        System.out.print(new String(array));
+        System.out.print(buffer.toString());
     }
 }
