7e224fbfc867d6920bb1a0718401cf40b47ccbd6
wangpeng <wangpeng>
2018-07-09 11:11:22 +0800

    wp
    自定义蜘蛛网雷达图

--- app/src/main/java/com/example/wp/wp_mvp_fragmentation/mvp/testviews/Main2Activity.java
+++ app/src/main/java/com/example/wp/wp_mvp_fragmentation/mvp/testviews/Main2Activity.java
@@ -1,10 +1,12 @@
 package com.example.wp.wp_mvp_fragmentation.mvp.testviews;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.wp.wp_mvp_fragmentation.R;
 import com.example.wp.wp_mvp_fragmentation.mvp.testviews.checkview.CheckView;
+import com.example.wp.wp_mvp_fragmentation.mvp.testviews.radarview.RadarView;
 
 public class Main2Activity extends AppCompatActivity {
     CheckView checkview;
@@ -13,6 +15,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        RadarView mRadarView = (RadarView) findViewById(R.id.radar);
+        mRadarView.setMainPaintColor(Color.BLUE);
+        mRadarView.setTextPaintColor(Color.GRAY);
+        mRadarView.setValuePaintColor(Color.RED);
     }
 }
--- app/src/main/java/com/example/wp/wp_mvp_fragmentation/mvp/testviews/radarview/RadarView.java
+++ app/src/main/java/com/example/wp/wp_mvp_fragmentation/mvp/testviews/radarview/RadarView.java
@@ -1,6 +1,10 @@
 package com.example.wp.wp_mvp_fragmentation.mvp.testviews.radarview;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,14 +14,198 @@ import android.view.View;
  */
 public class RadarView extends View {
     private int count = 6;//蜘蛛网格中正方形的个数
-    private float angle = (float) (Math.PI*2/count);//Math.PI记录的圆周率
+    private float angle = (float) (Math.PI * 2 / count);//Math.PI记录的圆周率
     private float radius;//网格最大半径
+    private int centerX, centerY;//中心xy
+    private String[] titles = {"a", "b", "c", "d", "e", "f"};
+    private double[] datas = {100, 60, 60, 60, 100, 10, 100, 10};
+    private float maxValue = 100;
+    private Paint mainPaint;//雷达区画笔
+    private Paint valuePaint;//数据区画笔
+    private Paint textPaint;//文本画笔
 
     public RadarView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public RadarView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        count = Math.min(titles.length, datas.length);
+
+        mainPaint = new Paint();
+        mainPaint.setAntiAlias(true);
+        mainPaint.setColor(Color.GRAY);
+        mainPaint.setStyle(Paint.Style.STROKE);
+
+        valuePaint = new Paint();
+        valuePaint.setAntiAlias(true);
+        valuePaint.setColor(Color.BLUE);
+        valuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        textPaint = new Paint();
+        textPaint.setTextSize(40);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setColor(Color.BLACK);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        radius = Math.min(w, h) / 2 * 0.9f;
+        //中心坐标
+        centerX = w / 2;
+        centerY = h / 2;
+        postInvalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawPolygon(canvas);//绘制正多边形
+        drawLine(canvas);//绘制从中心到末端的直线
+        drawText(canvas);//绘制文本
+        drawRegion(canvas);//绘制覆盖区域
+    }
+
+    /**
+     * 绘制蜘蛛网络
+     * 正多边形
+     *
+     * @param canvas
+     */
+    private void drawPolygon(Canvas canvas) {
+        Path path = new Path();
+        float r = radius / (count - 1);//r是蜘蛛丝之间的间距
+        for (int i = 0 ; i < count ; i ++ ){//中心点不用绘制
+            float curR = r * i;//当前正多边形的半径
+            path.reset();
+            for(int j = 0 ; j < count ; j ++){
+                if(j == 0){
+                    path.moveTo(centerX + curR,centerY);//起始位置
+                }else{
+                    /**
+                     * Math.sin(x)      x 的正玄值。返回值在 -1.0 到 1.0 之间；
+                     * Math.cos(x)    x 的余弦值。返回的是 -1.0 到 1.0 之间的数；
+                     */
+                    //根据半径，计算出蜘蛛丝上每个点的坐标
+                    float x = (float) (centerX + curR * Math.cos(angle * j));
+                    float y = (float) (centerY + curR * Math.sin(angle * j));
+                    path.lineTo(x,y);
+                }
+            }
+            path.close();//闭合路径
+            canvas.drawPath(path,mainPaint);
+        }
+    }
+
+    /**
+     * 根据半径计算出每个末端坐标
+     * 绘制从中心到末端的直线
+     *
+     * @param canvas
+     */
+    private void drawLine(Canvas canvas) {
+        Path path = new Path();
+        for (int i = 0; i < count; i++) {
+            path.reset();
+            path.moveTo(centerX, centerY);
+            float x = (float) (centerX + radius * Math.cos(angle * i));
+            float y = (float) (centerY + radius * Math.sin(angle * i));
+            path.lineTo(x, y);
+            canvas.drawPath(path, mainPaint);
+        }
+    }
+
+    /**
+     * 绘制文本
+     * 对于文本的绘制,首先要找到末端的坐标,由于末端和文本有一定的距离
+     * 给每个末端加上这个距离后,再绘制文本,另外，当文本在左边时，
+     * 由于不希望文本和蜘蛛网交叉，我们可以先计算出文本的长度，
+     * 然后使起始绘制坐标向左偏移这个长度。
+     *
+     * @param canvas
+     */
+    private void drawText(Canvas canvas) {
+        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+        float fontHeight = fontMetrics.descent - fontMetrics.ascent;
+        for (int i = 0; i < count; i++) {
+            float x = (float) (centerX + (radius + fontHeight / 2) * Math.cos(angle * i));
+            float y = (float) (centerY + (radius + fontHeight / 2) * Math.sin(angle * i));
+            if (angle * i >= 0 && angle * i <= Math.PI / 2) {//第4象限
+                canvas.drawText(titles[i], x, y, textPaint);
+            } else if (angle * i >= 3 * Math.PI / 2 && angle * i <= Math.PI * 2) {//第3象限
+                canvas.drawText(titles[i], x, y, textPaint);
+            } else if (angle * i > Math.PI / 2 && angle * i <= Math.PI) {//第2象限
+                float dis = textPaint.measureText(titles[i]);//文本长度
+                canvas.drawText(titles[i], x - dis, y, textPaint);
+            } else if (angle * i >= Math.PI && angle * i < 3 * Math.PI / 2) {//第1象限
+                float dis = textPaint.measureText(titles[i]);//文本长度
+                canvas.drawText(titles[i], x - dis, y, textPaint);
+            }
+        }
+    }
+
+    /**
+     * 绘制覆盖区域
+     * 覆盖区域，只要使用path记录下坐标点，然后设
+     * valuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+     *
+     * @param
+     */
+    private void drawRegion(Canvas canvas) {
+        Path path = new Path();
+        valuePaint.setAlpha(255);
+        for (int i = 0; i < count; i++) {
+            double percent = datas[i] / maxValue;
+            float x = (float) (centerX + radius * Math.cos(angle * i) * percent);
+            float y = (float) (centerY + radius * Math.sin(angle * i) * percent);
+            if (i == 0) {
+                path.moveTo(x, centerY);
+            } else {
+                path.lineTo(x, y);
+            }
+            //绘制小圆点
+            canvas.drawCircle(x, y, 10, valuePaint);
+        }
+        valuePaint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, valuePaint);
+        valuePaint.setAlpha(127);
+        //绘制填充区域
+        valuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawPath(path, valuePaint);
+    }
+
+    //设置标题
+    public void setTitles(String[] titles) {
+        this.titles = titles;
+    }
+
+    //设置数值
+    public void setData(double[] data) {
+        this.datas = data;
+    }
+
+    //设置最大数值
+    public void setMaxValue(float maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    //设置蜘蛛网颜色
+    public void setMainPaintColor(int color) {
+        mainPaint.setColor(color);
+    }
+
+    //设置标题颜色
+    public void setTextPaintColor(int color) {
+        textPaint.setColor(color);
+    }
+
+    //设置覆盖局域颜色
+    public void setValuePaintColor(int color) {
+        valuePaint.setColor(color);
     }
 }
